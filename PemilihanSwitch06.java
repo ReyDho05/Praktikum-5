ee89383be89c14f0fb6f2d08cfa6d3b02f627c59
ReyDho05 <rzulnisfian>
2024-09-24 15:10:44 +0700

    first-commit

---
+++ PemilihanSwitch06.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class PemilihanSwitch06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double angka1, angka2, hasil=0;
+        char operator;
+        System.out.print("Masukkan Angka 1: ");
+        angka1 = sc.nextDouble();
+        System.out.print("Masukkan Angka 2: ");
+        angka2 = sc.nextDouble();
+        System.out.print("Masukkan Operator (+ - * /): ");
+        operator = sc.next().charAt(0);
+        switch (operator) {
+            case '+':
+            hasil = angka1 + angka2;
+            break;
+            case '-':
+            hasil = angka1 - angka2;
+            break;
+            case '*' :
+            hasil = angka1*angka2;
+            break;
+            case '/':
+            hasil = angka1/angka2;
+            default:
+                break;
+        }
+        System.out.println(angka1 + " "+ operator +" "+ angka2 +" = "+ hasil);
+    }
+}
